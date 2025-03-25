3fb20931f5adb3b9608371f23634fb236a81c5de
morgan.zarka <morgan.zarka@opsone.net>
2025-03-25 10:23:57 +0100

    Use token instead of ips 0.3.0

--- app/controllers/stalker/application_controller.rb
+++ app/controllers/stalker/application_controller.rb
@@ -3,8 +3,9 @@
 module Stalker
   class ApplicationController < ActionController::API
     before_action do
-      client_ip = request.env.fetch(Stalker.configuration.client_ip_header, request.remote_ip)
-      head :unauthorized if Stalker.configuration.whitelist_ips.exclude?(client_ip)
+      auth_token = params[:token] 
+
+      head :unauthorized if Stalker.configuration.access_token && auth_token != Stalker.configuration.access_token
     end
   end
 end
--- lib/stalker.rb
+++ lib/stalker.rb
@@ -20,20 +20,16 @@ module Stalker
   end
 
   class Configuration
-    attr_accessor :client_ip_header, :whitelist_ips, :node_bin, :npm_bin, :yarn_bin, :bun_bin, :elasticsearch_url, :meilisearch_url
+    attr_accessor :client_ip_header, :access_token, :node_bin, :npm_bin, :yarn_bin, :bun_bin, :elasticsearch_url, :meilisearch_url
 
     def initialize
-      @whitelist_ips = []
+      @access_token = false
       @node_bin = 'node'
       @npm_bin = 'npm'
       @yarn_bin = 'yarn'
       @bun_bin = 'bun'
       @elasticsearch_url = ENV['ELASTICSEARCH_URL']
       @meilisearch_url = ENV['MEILISEARCH_URL']
-    end
-
-    def whitelist_ips
-      (%w[127.0.0.1 ::1] + @whitelist_ips).uniq
     end
   end
 end
