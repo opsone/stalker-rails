9fb7cbddcd508b32a9ee7091e68310270f1e4469
morgan.zarka <morgan.zarka@opsone.net>
2025-03-25 11:32:18 +0100

    change variable name

--- app/controllers/stalker/application_controller.rb
+++ app/controllers/stalker/application_controller.rb
@@ -3,9 +3,9 @@
 module Stalker
   class ApplicationController < ActionController::API
     before_action do
-      auth_token = params[:token].to_s
+      token = params[:token].to_s
 
-      if Stalker.configuration.access_token && !ActiveSupport::SecurityUtils(auth_token, Stalker.configuration.access_token.to_s)
+      if Stalker.configuration.access_token && !ActiveSupport::SecurityUtils.secure_compare(token, Stalker.configuration.access_token.to_s)
         head :unauthorized
       end
     end
