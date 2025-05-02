65b4709c53474eff8fb5403e604ad548dfbaf332
Kevyn Lebouille <kevyn.lebouille@opsone.net>
2025-05-02 17:20:48 +0200

    Update Linux probe

--- lib/stalker/linux.rb
+++ lib/stalker/linux.rb
@@ -11,9 +11,15 @@ module Stalker
     def self.version
       return unless enabled?
 
-      ini = IniFile.load('/etc/os-release', comment: '#', default: 'os', parameter: '=')
+      if File.exist?('/etc/debian_version')
+        version = `cat /etc/debian_version`.strip
 
-      { name: ini['os']['ID'], dep_type: 'os', version: ini['os']['VERSION_ID'] }
+        { name: 'debian', dep_type: 'os', version: version }
+      else
+        ini = IniFile.load('/etc/os-release', comment: '#', default: 'os', parameter: '=')
+
+        { name: ini['os']['ID'], dep_type: 'os', version: ini['os']['VERSION_ID'] }
+      end
     end
   end
 end
