340d87f8f6a4dbcb1a806a3ff28e41c9f02c6400
morgan.zarka <morgan.zarka@opsone.net>
2025-05-20 10:12:33 +0200

    change default configuration value

--- lib/generators/stalker/templates/initializer.rb
+++ lib/generators/stalker/templates/initializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Stalker.configure do |config|
-  # Keep it false if you want to disable the access token
-  config.access_token = false
+  # Set it to false if you want to disable the access token
+  config.access_token = Rails.application.credentials.stalker_token
 end
